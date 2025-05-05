c014b14bfe39c46773a550d17e290794ed92e955
Nicolas Pereira <nicolas.pp333@gmail.com>
2025-05-04 21:35:32 -0300

    Feat add About component and update Content layout for better structure

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,22 +1,25 @@
+import About from "../About/About";
 import "./Content.css";
 import { Route, Routes } from "react-router";
 
 function Content() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <div className="joke">
-            CSS <br />
-            IS AWESOME
-          </div>
-        }
-      />
-      <Route path="/about" element={<h1>About</h1>} />
-      <Route path="/projects" element={<h1>Projects</h1>} />
-      <Route path="/contact" element={<h1>Contact</h1>} />
-    </Routes>
+    <main className="content">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="joke">
+              CSS <br />
+              IS AWESOME
+            </div>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<h1>Projects</h1>} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </main>
   );
 }
 
